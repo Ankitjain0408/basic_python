1804b7e7db11bddcae02204ce9cfc933590f54c0
Ankitjain0408 <147256635+Ankitjain0408@users.noreply.github.com>
2023-10-07 19:11:02 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -31,11 +31,5 @@ print(sum)"""
 # print("positive:",positive)
 # print("negative:",negative)
 # print("zero:",zero)"""
-# num=int(input("enter a number"))
-# n=int(input("num:"))
-# f=1
-# for i in range(n+1):
-#     f=f*i
-#     sum+=f
-# print(sum)
+
     
